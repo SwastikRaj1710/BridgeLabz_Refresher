18af01f1d752844bbeb5619c2732253c95b3da69
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-15 18:27:16 +0530

    [Swastik] Added the ninth program which displays the execution time of the program

---
+++ ExecutionTime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExecutionTime // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startTime = DateTime.Now;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Program Running Time");
+            }
+            DateTime endTime = DateTime.Now;
+            TimeSpan totalTime = (endTime - startTime);
+            Console.WriteLine($"Execution time of the program:{totalTime.TotalMilliseconds}ms");
+        }
+    }
+}

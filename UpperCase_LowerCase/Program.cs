487d887c78f4e291d3f6eb0def922d9b583a6c37
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-15 13:13:07 +0530

    [Swastik] Added the third program which displays the name of my favourite place in UpperCase as well as LowerCase

---
+++ UpperCase_LowerCase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UpperCase_LowerCase // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name of your favourite place where you would like to visit the most:");
+            string place = Console.ReadLine();
+            Console.WriteLine("Place Name in Uppercase: " + place.ToUpper());
+            Console.WriteLine("Place Name in Lowercase: " + place.ToLower());
+        }
+    }
+}

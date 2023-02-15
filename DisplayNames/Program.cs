7fd4c07cde8fde98d0adc6618e768d28c36bb03d
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-15 13:03:09 +0530

    [Swastik] Added the first program which displays the names of 5 of my friends

---
+++ DisplayNames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DisplayNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here are the names of 5 of my friends:");
+            Console.WriteLine("Supriya");
+            Console.WriteLine("Udit");
+            Console.WriteLine("Abhay");
+            Console.WriteLine("Varun");
+            Console.WriteLine("Subhassini");
+        }
+    }
+}

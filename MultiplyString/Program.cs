204fb9b71ece5ecff47fed5c0e05d096d690f64e
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-15 13:17:53 +0530

    [Swastik] Added the fourth program which displays ten times the number entered by the user as a string

---
+++ MultiplyString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MultiplyString // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            String number = Console.ReadLine();
+            double ans = Convert.ToDouble(number) * 10;
+            Console.WriteLine("10 times the number " + number + " is equal to " + Convert.ToString(ans));
+        }
+    }
+}

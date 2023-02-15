740788fcfd64282f92c8ddeae4124787e89402ba
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-15 18:23:36 +0530

    [Swastik] Added the seventh program which takes the diameter of a circle as input and displays the perimeter and area of the circle

---
+++ Circle_Area_Perimeter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Circle_Area_Perimeter // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the diameter of the circle:");
+            double diameter = Convert.ToDouble(Console.ReadLine());
+            double radius = diameter / 2;
+            double perimeter = 2 * Math.PI * radius;
+            double area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine("Perimeter of the circle: " + perimeter);
+            Console.WriteLine("Area of the circle: " + area);
+        }
+    }
+}

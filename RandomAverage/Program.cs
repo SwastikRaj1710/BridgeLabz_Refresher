20ecce8eb2b5589833ce0e9da3e0d5a9d3a4027c
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-15 18:20:12 +0530

    [Swastik] Added the sixth program which displays the average of 5 randomly generated numbers between 10 and 50

---
+++ RandomAverage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomAverage // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int sum = 0;
+            double avg = 0;
+            Console.WriteLine("Five randomly generated numbers are: ");
+            for (int i = 0; i < 5; i++)
+            {
+                int num = random.Next(10, 51);
+                Console.Write(num + " ");
+                sum += num;
+            }
+            avg = sum / 5;
+            Console.WriteLine("\nAverage of the 5 above generated random numbers: " + avg);
+        }
+    }
+}

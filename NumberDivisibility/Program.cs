7418c748f3e7cb4ef65806943cf88ef9bc4eaac9
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-15 18:29:44 +0530

    [Swastik] Added the tenth program which displays all the numbers between 2000 and 3200 which are divisible by 7 but are not a multiple of 5

---
+++ NumberDivisibility/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumberDivisibility // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The numbers which are divisible by 7 and not a multiple of 5 between 2000 and 3200 are: ");
+            for (int i = 2000; i <= 3200; i++)
+            {
+                if (i % 7 == 0 && i % 5 != 0)
+                {
+                    Console.Write(i + ",");
+                }
+            }
+        }
+    }
+}

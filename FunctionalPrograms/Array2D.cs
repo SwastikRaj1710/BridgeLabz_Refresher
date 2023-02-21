9810d4513fcd861b0c2a391f3e760c3190b10111
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-21 14:42:42 +0530

    [Swastik] Added the 2D array program which takes as input all the array elements in a 2D Array and displays them

---
+++ FunctionalPrograms/Array2D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    internal class Array2D
+    {
+        public void Print2DArray(object[,] array)
+        {
+            Console.WriteLine("The elements of the 2D Array are:");
+            for(int i=0;i<array.GetLength(0);i++)
+            {
+                for(int j = 0; j < array.GetLength(1);j++)
+                {
+                    Console.WriteLine(array[i,j]);
+                }
+            }
+        }
+    }
+}
---
+++ FunctionalPrograms/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FunctionalPrograms // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. A library for reading in 2D arrays from standard input and printing them out to standard output");
+                Console.WriteLine("2. Exit");
+                Console.WriteLine("Enter your choice");
+                int ch = Convert.ToInt32(Console.ReadLine());
+                
+                switch(ch)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the numner of rows and columns");
+                        int m = Convert.ToInt32(Console.ReadLine());
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        object[,] arr1 = new object[m,n];
+                        Console.WriteLine("Enter the array elements");
+                        for(int i=0;i<m;i++)
+                        {
+                            for(int j=0;j<n;j++)
+                            {
+                                arr1[i,j] = Console.ReadLine();
+                            }
+                        }
+                        Array2D obj1 = new Array2D();
+                        obj1.Print2DArray(arr1);
+                        break;
+                    case 2:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}

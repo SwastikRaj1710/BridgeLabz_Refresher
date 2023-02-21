fc252f1a0281e5b31c1f1de6439910e41a987efb
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-21 20:17:11 +0530

    [Swastik] Added the Gambling program which calculates the total number of wins and the win loss percentages for the given stake, goal and number of bets

---
+++ LogicalPrograms/Gambler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Gambler
+    {
+        public void Gamble(int stake, int goal, int bets)
+        {
+            int winCount = 0;
+            Random random = new Random();
+            for(int i = 0; i < bets;i++)
+            {
+                int betResult = random.Next(0, 2);
+                if (betResult == 1)
+                {
+                    winCount++;
+                    stake--;
+                }
+                else
+                {
+                    stake--;
+                }
+                if(stake == 0 || stake==goal)
+                {
+                    break;
+                }
+            }
+            double winPercent = winCount * 100 / bets;
+            Console.WriteLine("Number of wins: " + winCount);
+            Console.WriteLine("Win Percentage: " + winPercent);
+            Console.WriteLine("Loss Percentage: " + (100 - winPercent));
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LogicalPrograms // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. A Gambler Simulation which displays the number of wins and win-loss percentage");
+                Console.WriteLine("2. Exit");
+                Console.WriteLine("Enter your choice");
+                int ch = Convert.ToInt32(Console.ReadLine());
+
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the stake");
+                        int stake = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the goal");
+                        int goal = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the number of bets");
+                        int bets = Convert.ToInt32(Console.ReadLine());
+                        Gambler obj1 = new Gambler();
+                        obj1.Gamble(stake, goal, bets);
+                        break;
+                    case 2:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}

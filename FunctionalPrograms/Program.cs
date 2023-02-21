04abf4e835f0f2935a6ece94a81dceb1a844e895
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-21 16:05:03 +0530

    [Swastik] Added the Zero Sum triplet program which displays the triplets in a given array whose sum is equal to zero

--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -10,7 +10,8 @@ namespace FunctionalPrograms // Note: actual namespace depends on the project na
             {
                 Console.WriteLine("\nMenu:");
                 Console.WriteLine("1. A library for reading in 2D arrays from standard input and printing them out to standard output");
-                Console.WriteLine("2. Exit");
+                Console.WriteLine("2. Count the number of triples that sum to exactly 0");
+                Console.WriteLine("3. Exit");
                 Console.WriteLine("Enter your choice");
                 int ch = Convert.ToInt32(Console.ReadLine());
                 
@@ -20,19 +21,39 @@ namespace FunctionalPrograms // Note: actual namespace depends on the project na
                         Console.WriteLine("Enter the numner of rows and columns");
                         int m = Convert.ToInt32(Console.ReadLine());
                         int n = Convert.ToInt32(Console.ReadLine());
-                        object[,] arr1 = new object[m,n];
+                        object[,] arr = new object[m,n];
                         Console.WriteLine("Enter the array elements");
                         for(int i=0;i<m;i++)
                         {
                             for(int j=0;j<n;j++)
                             {
-                                arr1[i,j] = Console.ReadLine();
+                                arr[i,j] = Console.ReadLine();
                             }
                         }
                         Array2D obj1 = new Array2D();
-                        obj1.Print2DArray(arr1);
+                        obj1.Print2DArray(arr);
                         break;
                     case 2:
+                        Console.WriteLine("Enter the number of elements");
+                        int len1 = Convert.ToInt32(Console.ReadLine());
+                        int[] arr1 = new int[len1];
+                        Console.WriteLine("Enter the array elements");
+                        for(int i=0;i<len1;i++)
+                        {
+                            arr1[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Triplets obj2 = new Triplets();
+                        int tripletsCount = obj2.FindTriplets(arr1);
+                        if(tripletsCount==0)
+                        {
+                            Console.WriteLine("No triplets found");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Number of triplets found " + tripletsCount);
+                        }
+                        break;
+                    case 3:
                         Environment.Exit(0);
                         break;
                 }
---
+++ FunctionalPrograms/Triplets.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    internal class Triplets
+    {
+        public int FindTriplets(int[] array)
+        {
+            Console.WriteLine("The triplets having sum equal to zero are:");
+            int count = 0;
+            Array.Sort(array);
+            for(int i=0;i<array.Length;i++)
+            {
+                if (i > 0 && array[i] == array[i - 1]) 
+                    continue;
+                for (int j=i+1;j<array.Length;j++)
+                {
+                    if (j > i + 1 && array[j] == array[j - 1]) 
+                        continue;
+                    for (int k=j+1;k<array.Length;k++)
+                    {
+                        if (k > j + 1 && array[k] == array[k - 1])
+                            continue;
+                        if (array[i] + array[j] + array[k] == 0)
+                        {
+                            count++;
+                            Console.WriteLine(array[i] + " " + array[j] + " " + array[k]);
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}

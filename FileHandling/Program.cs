1189b23441cf7a420f8b7109f15c6196af26c511
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-21 23:52:54 +0530

    [Swastik] Added the FileHandling program which performs file reading and writing operations on a .txt file

---
+++ FileHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FileHandling // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Perform read write operations on a TXT file");
+                Console.WriteLine("2. Perform read write operations on a CSV file");
+                Console.WriteLine("3. Perform read write operations on a JSON file");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("Enter your choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 1:
+                        TXT obj1 = new TXT();
+                        obj1.ReadWriteOps();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect Choice!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ FileHandling/TXT.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling
+{
+    internal class TXT
+    {
+        public void ReadWriteOps()
+        {
+            Console.WriteLine("Enter the Employee details");
+            Console.WriteLine("Enter the First name");
+            string fname = Console.ReadLine();
+            Console.WriteLine("Enter the Last name");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter the Phone number");
+            string phone = Console.ReadLine();
+            Console.WriteLine("Enter the Email id");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("\nWriting the details to the file...");
+
+            StreamWriter sw = new StreamWriter("D:\\BridgeLabz\\BridgeLabz_Refresher\\File1.txt");
+            sw.WriteLine("First Name: " + fname);
+            sw.WriteLine("Last Name: " + lname);
+            sw.WriteLine("Address: " + address);
+            sw.WriteLine("Phone Number: " + phone);
+            sw.WriteLine("Email ID: " + email);
+            sw.Close();
+
+            Console.WriteLine("\nReading the details from the file...");
+
+            StreamReader sr = new StreamReader("D:\\BridgeLabz\\BridgeLabz_Refresher\\File1.txt");
+            string line = sr.ReadLine();
+            while(line!=null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+        }
+    }
+}

be5f5d6251782fbaaad445ea303eb489fe2ea7fa
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-22 20:58:59 +0530

    [Swastik] Added the FileHandling program which performs file reading and writing operations on a .json file

---
+++ FileHandling/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace FileHandling
+{
+    class Employee
+    {
+        public string firstName {  get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string emailid { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Employee Details: \nFirstName: {0}\nLastName: {1}\nAddress: {2}\nPhoneNumber: {3}\nEmailID: {4}\n", firstName, lastName, address, phone, emailid);  
+        }
+
+    }
+}
---
+++ FileHandling/JSON.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FileHandling
+{
+    internal class JSON
+    {
+        public void ReadWriteOps()
+        {
+            Console.WriteLine("Enter the Employee details");
+            Console.WriteLine("Enter the First name");
+            string fname = Console.ReadLine();
+            Console.WriteLine("Enter the Last name");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            string add = Console.ReadLine();
+            Console.WriteLine("Enter the Phone number");
+            string ph = Console.ReadLine();
+            Console.WriteLine("Enter the Email id");
+            string email = Console.ReadLine();
+
+            Employee emp = new Employee()
+            {
+                firstName = fname,
+                lastName = lname,
+                address = add,
+                phone = ph,
+                emailid = email
+            };
+
+            string ResultJson = JsonConvert.SerializeObject(emp);
+            Console.WriteLine("\nWriting the details to the file...");
+            string jsonPath = "D:\\BridgeLabz\\BridgeLabz_Refresher\\File1.json";
+            File.WriteAllText(jsonPath, ResultJson);
+
+            string ResultString = File.ReadAllText(jsonPath);
+            Employee Result = JsonConvert.DeserializeObject<Employee>(ResultString);
+            Console.WriteLine(Result.ToString());
+        }
+    }
+}

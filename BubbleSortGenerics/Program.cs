ae61cd947ecdab3bb2cf2208db28817056c4a48d
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-23 09:39:59 +0530

    [Swastik] Added the BubbleSort program which sorts a given array of any data type using Generics

---
+++ BubbleSortGenerics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BubbleSortGenerics // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 4, 5, 7, 6, 9, 4, 1, 1, 3, 4, 50, 56, 41 };
+            var sortedArr = BubbleSort(arr);
+            Console.WriteLine("Sorterd Array:");
+            foreach(int i in sortedArr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public static IEnumerable<T> BubbleSort<T>(IEnumerable<T> array) where T : IComparable
+        {
+            T[] arr = array.ToArray();
+            T temp;
+            int size = arr.Length;
+            for(int i=0; i < size-1; i++)
+            {
+                for(int j=0; j<size-1-i; j++)
+                {
+                    if (arr[j].CompareTo(arr[j + 1]) > 0)
+                    {
+                        temp  = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}

8362c44a57cd55e8b32d3531ab706b62c5ab764e
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-23 17:58:20 +0530

    [Swastik] Added the Balanced Parenthesis program which finds out whether a given expression has balanced set of brackets or not

---
+++ DataStructurePrograms/BalancedParenthesis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePrograms
+{
+
+    internal class BalancedParenthesis
+    {
+        public bool IsBalanced(char[] expression)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                    stack.Push(expression[i]);
+
+                if (expression[i] == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    stack.Pop();
+                }
+            }
+            if (stack.Count == 0)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -10,7 +10,8 @@ namespace DataStructurePrograms // Note: actual namespace depends on the project
             {
                 Console.WriteLine("\nMenu:");
                 Console.WriteLine("1. Read data from a file, perform insertion or deletion and write the data back to another file");
-                Console.WriteLine("2. Exit");
+                Console.WriteLine("2. Check for Balanced Parenthesis");
+                Console.WriteLine("3. Exit");
                 
                 Console.WriteLine("Enter your choice");
                 int ch = Convert.ToInt32(Console.ReadLine());
@@ -22,6 +23,23 @@ namespace DataStructurePrograms // Note: actual namespace depends on the project
                         obj1.FileModification();
                         break;
                     case 2:
+                        Console.WriteLine("Enter the expression");
+                        string expression = Console.ReadLine();
+                        char[] brackets = new char[expression.Length];
+                        int k = 0;
+                        for(int i=0;i<expression.Length;i++)
+                        {
+                            if (expression[i] == '(' || expression[i] == ')')
+                            {
+                                brackets[k++] = expression[i];
+                            }
+                        }
+                        BalancedParenthesis obj2 = new BalancedParenthesis();
+                        bool result = obj2.IsBalanced(brackets);
+                        if(result)
+                            Console.WriteLine("The given expression has Balanced Parenthesis");
+                        else
+                            Console.WriteLine("The given expression does not have Balanced Parenthesis");
                         Environment.Exit(0);
                         break;
                     default:

ea89713b8810053d35dcc356aa94f015f6b4a554
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-02-25 14:31:30 +0530

    [Swastik] Added the Stock Account Management program which takes as input the details of various stocks and calculates the total value of each stock as well as total value of all stocks owned

---
+++ ObjectOrientedPrograms/Stock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPrograms
+{
+    internal class Stock
+    {
+        public string name;
+        public int noOfShares;
+        public double price;
+
+        public Stock(string stockName, int nShares, double stockPrice)
+        {
+            name = stockName;
+            noOfShares = nShares;
+            price = stockPrice;
+        }
+        public double ValueOfStock()
+        {
+            double total = this.noOfShares * this.price;
+            return total;
+        }
+
+        public void PrintValue()
+        {
+            Console.WriteLine("Value of " + this.name + " = " + this.ValueOfStock());
+        }
+
+    }
+}
---
+++ ObjectOrientedPrograms/StockPortfolio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPrograms
+{
+    internal class StockPortfolio
+    {
+        public void StockDetails()
+        {
+            Console.WriteLine("Enter the number of stocks");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Stock[] stocks = new Stock[num];
+            for (int i=0;i<num;i++)
+            {
+                Console.WriteLine("Enter the stock namne, number of shares and share price");
+                string stockName = Console.ReadLine();
+                int nofShares = Convert.ToInt32(Console.ReadLine());
+                double sharepPrice = Convert.ToDouble(Console.ReadLine());
+                Stock stock = new Stock(stockName,nofShares,sharepPrice);
+                stocks[i] = stock;
+            }
+            double totalValue = 0;
+
+            for(int i=0;i<num;i++)
+            {
+                totalValue+= stocks[i].ValueOfStock();
+                stocks[i].PrintValue();
+            }
+
+            Console.WriteLine("Total Value of all the Stocks owned: " + totalValue);
+
+        }
+    }
+}
